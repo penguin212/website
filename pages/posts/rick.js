7e314befff3e788c9730164352798197fc0e6a65
dylan-huber <85318362+penguin212@users.noreply.github.com>
2022-08-08 19:10:22 -0700

    adding the roll

--- pages/posts/rick.js
+++ pages/posts/rick.js
@@ -5,13 +5,20 @@ export default function rick() {
     return (
         <>
             <h1> omg welcome to my website</h1>
-            <p> this is so cool right \n
-                here is cool image right \n
+            <p> this is so cool right <br/>
+                here is cool image right <br/>
                 she likes to collect rocks </p>
-            <Image 
-                src="\images\IMG_20220807_092716.jpg"
-                height={250}
-                width={1000} />
+            <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                <Image 
+                    src="/images/IMG_20220807_092716.jpg"
+                    height={250}
+                    width={1000} />
+            </Link>
+            <p>
+                she has something to show you <br/>
+                she want you to see her rock collection <br/>
+                click on her and she will bring you to it :D <br/>
+            </p>
         </>
     )
 }
